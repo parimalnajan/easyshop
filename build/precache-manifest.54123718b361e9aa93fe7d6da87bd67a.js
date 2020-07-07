eaa769b53c5ac0b9ac6187bb985ddfe6125de5ad
parimalnajan <63017672+parimalnajan@users.noreply.github.com>
2020-07-07 04:34:38 -0700

    Add files via upload

---
+++ build/precache-manifest.54123718b361e9aa93fe7d6da87bd67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83694561d418310b82d6bf8bc27f6842",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f60f72c1c8fa77a530",
+    "url": "/static/css/main.955cdaca.chunk.css"
+  },
+  {
+    "revision": "7a8bf6167eb7515f43a8",
+    "url": "/static/js/2.41071d03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.41071d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f60f72c1c8fa77a530",
+    "url": "/static/js/main.9c249214.chunk.js"
+  },
+  {
+    "revision": "b99d21a4f4a143876279",
+    "url": "/static/js/runtime-main.0defbde4.js"
+  }
+]);
